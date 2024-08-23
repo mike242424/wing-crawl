ec4a66ea2f4f94743ceab9e6c958868b54d3d211
Michael Byers <mikebyers24@gmail.com>
2024-08-23 13:49:43 -0400

    navbar styling.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -19,9 +19,9 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className="bg-primary p-4 shadow-md">
+    <nav className="bg-primary py-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
-        <Link href="/" className="font-bold text-white text-xl tracking-wider">
+        <Link href="/" className="font-bold text-white text-xl">
           Wing Crawl 2024
         </Link>
         {isSignedUp && (
